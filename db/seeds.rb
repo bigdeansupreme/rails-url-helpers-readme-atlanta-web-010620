b73951c2714c18096f6af7657d6d7af134eaad68
Dino <dinokunovac@gmail.com>
2020-01-30 16:27:17 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+post = Post.new(title: 'A lovely title', description: 'A superb description')
+post.save
